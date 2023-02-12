21f16b78e6c276ce90cf13967a2dc84646c58497
aakashacharjee <aakashacharjee0@gmail.com>
2023-02-13 00:53:40 +0530

    v1

---
+++ wp-content/uploads/custom-css-js/612.js
@@ -0,0 +1,2 @@
+<!-- start Simple Custom CSS and JS -->
+<script>(function(w, d) { w.CollectId = "62f3b391fb91d0332b8a9521"; var h = d.head || d.getElementsByTagName("head")[0]; var s = d.createElement("script"); s.setAttribute("type", "text/javascript"); s.async=true; s.setAttribute("src", "https://collectcdn.com/launcher.js"); h.appendChild(s); })(window, document);</script><!-- end Simple Custom CSS and JS -->
